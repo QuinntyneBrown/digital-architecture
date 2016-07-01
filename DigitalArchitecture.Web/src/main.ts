29725e169313e12212c43bc47177c2c2a188e9e3
Quinntyne Brown <quinntynebrown@gmail.com>
2016-07-01 15:37:13 -0400

    organization

--- DigitalArchitecture.Web/src/main.ts
+++ DigitalArchitecture.Web/src/main.ts
@@ -1,7 +1,7 @@
 ﻿/// <reference path="../ts/rx.all.d.ts" />
 /// <reference path="../ts/angular-rx-ui.d.ts" />
 
-import { HomeContainerComponent } from "./home/home-container.component";
+import { HomeContainerComponent } from "./app/home/home-container.component";
 
 var app = angular.module("digitalArchitectureApp", ["components"]) as any;
 
