5d0df2bddd7753fbf7d3d79fa1da11f8b4600927
Quinntyne Brown <quinntynebrown@gmail.com>
2016-07-09 19:23:34 -0400

    DbMigrator

--- DigitalArchitecture.Web/src/app/routing/authorization-required-guard.ts
+++ DigitalArchitecture.Web/src/app/routing/authorization-required-guard.ts
@@ -1,7 +1,7 @@
 ﻿export const authorizationRequiredGuard = {
     route: "*",
     promise: ["loginRedirect", "$q", "$route", "invokeAsync", "store", "userActionCreator", (loginRedirect, $q: angular.IQService, $route, invokeAsync, store: any, userActionCreator: any) => {
-        var deferred = $q.defer();
+        let deferred = $q.defer();
         invokeAsync(userActionCreator.current).then(results => {
             if ($route.current.$$route.authorizationRequired && !(store.getValue() as any).currentUser) {
                 loginRedirect.redirectToLogin();
---
+++ DigitalArchitecture/App_Start/AppActivator.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity.Migrations;
+using WebActivatorEx;
+
+[assembly: PostApplicationStartMethod(typeof(DigitalArchitecture.AppActivator), "PostStart")]
+
+namespace DigitalArchitecture
+{
+    public class AppActivator
+    {
+        public static void PostStart()
+        {
+            var dbMigrator = new DbMigrator(new Migrations.Configuration());
+            dbMigrator.Update();
+        }
+    }
+}
