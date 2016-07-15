bb642fea379e7f7606adaf0afce0e57715feeafa
Quinntyne Brown <quinntynebrown@gmail.com>
2016-07-14 23:49:53 -0400

    login

--- DigitalArchitecture.Web/src/app/login/login-container.component.ts
+++ DigitalArchitecture.Web/src/app/login/login-container.component.ts
@@ -1,9 +1,17 @@
-import { CanActivate, ChangeDetectionStrategy, Component } from "angular-rx-ui/src/components/core";
+import { CanActivate, ChangeDetectionStrategy, Component, ILoginRedirect } from "angular-rx-ui/src/components/core";
+import { UserLoggedInAction } from "angular-rx-ui/src/components/login/login.actions";
 
 @Component({
     template: require("./login-container.component.html"),
     styles: [require("./login-container.component.scss")],
     selector: "da-login-container",
+    viewProviders: ["loginRedirect"],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginContainerComponent { }
+export class LoginContainerComponent {
+    constructor(private loginRedirect: ILoginRedirect) { }
+    storeOnChange = state => {
+        if (state.lastTriggeredByActionType == "login.userLoggedInAction")
+            this.loginRedirect.redirectPreLogin();
+    }
+}
