5a1d3fd66dee73881e6bbb9f01d89cff6e637687
Quinntyne Brown <quinntynebrown@gmail.com>
2016-07-18 21:08:37 -0400

    app-guard

---
+++ DigitalArchitecture.Web/src/app/routing/app-guard.ts
@@ -0,0 +1,19 @@
+﻿import { IRoutePromise } from "angular-rx-ui/src/components/core/route-resolver";
+import { AppActionCreator } from "../content-aggregation/action-creators/app.action-creator";
+
+export let appGuard: IRoutePromise = {
+    promise: [
+    "$q",
+    "$route",
+    "invokeAsync",
+    "appActionCreator",
+    (
+        $q: angular.IQService,
+        $route: angular.route.IRouteService,
+        invokeAsync,
+        appActionCreator: AppActionCreator) => invokeAsync(appActionCreator.all)
+    ],
+    route:"*"
+};
+
+
